a0f77728bcd2b42e8b291f961a4babe049520542
Alfred Zimo <157354657+zmxlfrd@users.noreply.github.com>
2024-11-04 13:47:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links"); 
+    const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
 
 function sendMail() {
-    const params = {
+    var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value
     };
 
-    emailjs
-        .send("service_20pigze", "template_jm79tde", params)
+    emailjs.send("service_20pigze", "template_jm79tde", params)
         .then((res) => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
             document.getElementById("message").value = "";
-            console.log("Response:", res);
+            console.log(res);
             alert("Message sent successfully!");
         })
-        .catch((err) => {
-            console.error("Error:", err);
-            alert("Failed to send the message. Please try again later.");
-        });
+        .catch((err) => console.log(err));
 }
